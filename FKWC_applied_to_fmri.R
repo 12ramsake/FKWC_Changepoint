5e9b9084c7a170961a9278264ad24b86578ee79d
12ramsake <32965328+12ramsake@users.noreply.github.com>
2021-08-18 15:10:41 -0500

    Add files via upload

---
+++ FKWC_applied_to_fmri.R
@@ -0,0 +1,127 @@
+############ Perform FKWC on imaging data
+#You need the package numDeriv
+# imag_array is a four dimensional array of the f-mri images, where the fourth dimension is the time in the sequence
+#
+
+DIM1=dim(imag_array)[1]
+DIM2=dim(imag_array)[2]
+DIM3=dim(imag_array)[3]
+
+
+#generate a 3 dimensional unit function
+generate_unit_function=function(dim_img=c(DIM1,DIM2,DIM3)){
+  uv_func=array(rnorm(prod(dim_img)),dim=dim_img)
+  return(uv_func)
+}
+
+#num random direnctions
+M=50
+random_directions=replicate(M,generate_unit_function())
+
+#project a 3d image onto unit function
+project=function(uv,img3d){
+  num_bf=dim(uv)[1]
+  proj=sum(uv*img3d)
+  return(proj)
+}
+
+
+#compute projections of regular data
+projections=sapply(1:M,function(rd){apply(imag_array,4,function(x){project(random_directions[,,,rd],x)})}); projections
+
+#compute derivatives and their projections
+pic=function(x,pic_num=1){
+  # print(x)
+  xx=floor(x[1]*DIM1)
+  yy=floor(x[2]*DIM2)
+  zz=floor(x[3]*DIM3)
+  if(xx<1)
+    xx=1
+  if(zz<1)
+    zz=1
+  if(yy<1)
+    yy=1
+  
+  if(xx>DIM1)
+    xx=DIM1
+  if(zz>DIM3)
+    zz=DIM3
+  if(yy>DIM2)
+    yy=DIM2
+  
+  
+  return(imag_array[xx,yy,zz,pic_num] )
+  
+}
+
+all_pnts=expand.grid((1:DIM1)/DIM1,(1:DIM2)/DIM2,(1:DIM3)/DIM3)
+all_pnts2=expand.grid((1:DIM1),(1:DIM2),(1:DIM3))
+
+get_derivatives=function(pic_num){
+  
+  deriv1=apply(all_pnts,1,function(z){numDeriv::grad(pic,unlist(z, use.names=FALSE),
+                                                     method="Richardson",
+                                                     method.args = list(eps=0.01,d=.1)
+                                                     ,pic_num=pic_num)})
+  
+  dxx=dyy=dzz=imag_array[,,,pic_num]
+  
+  for(x in 1:nrow(all_pnts2)){
+    y=unlist(all_pnts2[x,], use.names=FALSE);  
+    dxx[y[1],y[2],y[3]] = deriv1[1,x]
+    dyy[y[1],y[2],y[3]] = deriv1[2,x]
+    dzz[y[1],y[2],y[3]] = deriv1[3,x]
+  }
+  
+  return(list(dxx,dyy,dzz))
+  
+}
+
+num_scans=dim(imag_array)[4]
+derivs=sapply(1:num_scans,get_derivatives)
+
+
+
+
+
+# 
+derivsx2=imag_array
+derivsy2=imag_array
+derivsz2=imag_array
+
+for(i in 1:(dim(imag_array)[4])){
+  derivsx2[,,,i]=derivs[1,i][[1]]
+  derivsy2[,,,i]=derivs[2,i][[1]]
+  derivsz2[,,,i]=derivs[3,i][[1]]
+}
+
+projections_derivative_x=sapply(1:M,function(rd){apply(derivsx2,4,function(x){project(random_directions[,,,rd],x)})}); projections_derivative_x
+projections_derivative_y=sapply(1:M,function(rd){apply(derivsy2,4,function(x){project(random_directions[,,,rd],x)})}); projections_derivative_y
+projections_derivative_z=sapply(1:M,function(rd){apply(derivsz2,4,function(x){project(random_directions[,,,rd],x)})}); projections_derivative_z
+
+
+
+
+
+
+
+
+#for with derivatives, d_vals is depth values
+d_vals=sapply(1:M,function(x){ddalpha::depth.halfspace(cbind(projections[,x],projections_derivative_x[,x],projections_derivative_y[,x],projections_derivative_z[,x]),
+                                                       cbind(projections[,x],projections_derivative_x[,x],projections_derivative_y[,x],projections_derivative_z[,x])) })
+
+
+plot(d_vals,type='l')
+ranks=rank(rowMeans(d_vals))
+
+# Rcpp::sourceCpp('PELT_CPP.cpp')
+
+source('FKWC Methods.R')
+
+cp=PELT_T(ranks,.3*sqrt(dim(imag_array))+3.74)
+cp
+
+#run epidemic test
+run_test(ranks)
+
+
